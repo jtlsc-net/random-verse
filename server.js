73ca9ef1cd9007375a994181afc3d801e70eb09f
jtlsc-net <jlumshuechan@ufl.edu>
2020-12-04 20:03:52 -0500

    Ok pages

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const express = require('express');
-const path = require('path');const app = express();
+const path = require('path');
+const app = express();
 const API_KEY = '69046020591553883e007be21a631c25';
 app.use(express.json());
 app.use(express.static("express"));// default URL for website
 app.use('/', function(req,res){
-    let versePath = require('./express/verses.json');
+    let versePath = require('./verses.json');
     res.sendFile(path.join(__dirname+'/express/index.html'));
     res.send(JSON.stringify(versePath));
     //__dirname : It will resolve to your project folder.
